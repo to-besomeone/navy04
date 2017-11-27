d77bcaaa9ae3d872659a3a45fc335f744ffe7b94
to-besomeone <31721777+to-besomeone@users.noreply.github.com>
2017-11-27 15:55:19 +0900

    economics html 2nd edit
    html change
    css change

--- js/index.js
+++ js/index.js
@@ -5,16 +5,16 @@
   slideContainer.slick();
   
   $('.clash-card__image img').hide();
-  $('.slick-active').find('.clash-card img').fadeIn(200);
+  $('.slick-active').find('.clash-card img').fadeIn(0);
   
   // On before slide change
   slideContainer.on('beforeChange', function(event, slick, currentSlide, nextSlide) {
-    $('.slick-active').find('.clash-card img').fadeOut(1000);
+    $('.slick-active').find('.clash-card img').fadeOut(0);
   });
   
   // On after slide change
   slideContainer.on('afterChange', function(event, slick, currentSlide) {
-    $('.slick-active').find('.clash-card img').fadeIn(200);
+    $('.slick-active').find('.clash-card img').fadeIn(0);
   });
   
 })();
