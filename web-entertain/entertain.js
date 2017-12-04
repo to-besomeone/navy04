93a670798dfef115b847a04b8d0df8a6f19d7fda
to-besomeone <31721777+to-besomeone@users.noreply.github.com>
2017-12-04 22:24:58 +0900

    organize entertain

---
+++ web-entertain/entertain.js
@@ -0,0 +1,20 @@
+(function() {
+  
+  var slideContainer = $('.slide-container');
+  
+  slideContainer.slick();
+  
+  $('.clash-card__image img').hide();
+  $('.slick-active').find('.clash-card img').fadeIn(1000);
+  
+  // On before slide change
+  slideContainer.on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+    $('.slick-active').find('.clash-card img').fadeOut(3500);
+  });
+  
+  // On after slide change
+  slideContainer.on('afterChange', function(event, slick, currentSlide) {
+    $('.slick-active').find('.clash-card img').fadeIn(1000);
+  });
+  
+})();
